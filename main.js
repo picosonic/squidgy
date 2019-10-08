4fe78c25a131dbb2bbc1c0c5c8659f4baf240b10
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2019-10-08 14:02:27 +0100

    Added playfield and level viewer

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+var spritewidth=24;
+var spriteheight=32;
+var levelwidth=14;
+var levelheight=18;
+
+var level=1;
+
+function drawlevel()
+{
+  var x, y;
+  var content="";
+
+  for (y=0; y<levelheight; y++)
+  {
+    for (x=0; x<levelwidth; x++)
+    {
+      var sprite=0;
+
+      switch (levels[level].data[(y*levelwidth)+x])
+      {
+        case ' ': sprite=0; break; // Space
+        case 'W': sprite=1; break; // Wall
+        case '.': sprite=2; break; // Dot
+        case 'S': sprite=4; break; // Squidgy
+        case 'H': sprite=6; break; // Horizontal enemy
+        case 'V': sprite=7; break; // Vertical enemy
+        case 'X': sprite=8; break; // Exit
+        case 'w': sprite=9; break; // Bomb
+        case '^': sprite=10; break; // Wall curved top
+        case 'v': sprite=11; break; // Wall curved bottom
+        case 'B': sprite=12; break; // ?? Not sure ??
+        default: sprite=levels[level].data.charCodeAt((y*levelwidth)+x)-0x30; break;
+      }
+
+      content+="<div class='tile s"+sprite+"'></div>";
+    }
+  }
+
+  document.getElementById("playfield").innerHTML=content;
+}
+
 // Entry point
 function init()
 {
@@ -7,6 +48,8 @@ function init()
     e = e || window.event;
     e.preventDefault();
   };
+
+  drawlevel();
 }
 
 // Run the init() once page has loaded
